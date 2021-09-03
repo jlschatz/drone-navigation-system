c9315154bc64d09be793a9437fceccfc30ed229e
Jurgen Schatz <jurgen.schatz@standardbank.co.za>
2021-09-03 13:54:44 +0200

    Unittests and extra documentation

--- service/dns-service_test.go
+++ service/dns-service_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"os"
 	"testing"
 
 	"github.com/drone-navigation-system/models"
@@ -8,6 +9,8 @@ import (
 )
 
 func TestDNSService_GetLocation(t *testing.T) {
+
+	os.Setenv("SECTOR_ID", "5.0")
 
 	req := models.Request{X: "534.0", Y: "77.00", Z: "898.0", Vel: "66.6"}
 	resp := models.Response{Location: 7611.6}
